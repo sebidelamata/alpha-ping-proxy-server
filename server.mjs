1dc5dba3a303692b0279f57331e994173454cc1a
Miguel  de la Mata <sebidelamata@gmail.com>
2024-05-12 22:11:06 -0600

    initial comit

---
+++ server.mjs
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config();
+import fetch from 'node-fetch';
+
+app.use(cors({
+    origin: '*'
+}))
+
+app.get('/token-metadata/:tokenAddress', async(req, res) => {
+    const tokenAddress = req.params.tokenAddress;
+    console.log(tokenAddress)
+    const url=` https://pro-api.coinmarketcap.com/v2/cryptocurrency/info?address=${tokenAddress}`
+    const response = await fetch(
+        url,
+        {
+        headers: {
+        'Accepts': 'application/json',
+        "X-CMC_PRO_API_KEY": process.env.COINMARKETCAP_API_KEY
+        }
+    }
+    ) 
+    res.json(
+        await response.json()
+    )
+})
+
+const PORT = process.env.PORT || 443;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
